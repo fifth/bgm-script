ccbeddeda30e9ef16357123165a55cb2cd7e7a25
北上 <beishang@yiran.com>
2017-02-20 14:26:56 +0800

    using cheerio

--- app.js
+++ app.js
@@ -1,52 +1,66 @@
 var express = require('express');
 var superagent = require('superagent');
+var cheerio = require('cheerio');
 
 var app = express();
 
 function findID (e) {
-        return e.split('/')[3];
+    return e.split('/')[3];
 }
 
 app.get(/^\/bgm\-api\/getFriendsList\/\w+/, function (req, res) {
-        var uid = findID(req.url);
-        superagent
-                .get('http://bangumi.tv/user/' + uid + '/friends')
-                .end(function (err, data) {
-                        var txt = data.text;
-                        var friendsList = [];
-                        txt.match(/\<a\shref\=\"\/user\/\w+\"/g).forEach(function (item) {
-                                var name = item.match(/\w+/g)[3];
-                                if (name != uid) {
-                                        friendsList.push(name);
-                                }
-                        });
-                        res.send(friendsList);
-                });
+    // var uid = findID(req._parsedUrl.pathname);
+    var uid = findID(req.url);
+    // var fullInfo = req.query.fullInfo || 0;
+    var fullInfo = 0;
+    superagent
+        .get('http://bangumi.tv/user/' + uid + '/friends')
+        .end(function (err, data) {
+            var txt = data.text;
+            var $ = cheerio.load(txt);
+            var friendsList = {};
+            $('li.user a').each(function (i, elem) {
+                var fid = $(this).attr('href').match(/\w+/g)[1];
+                var name = $(this).text().replace(/\s+/g,'');
+                var img = cheerio.load($(this).html())('img').attr('src');
+                friendsList[fid] = {
+                    name: name,
+                    img: img
+                }
+            });
+            if (fullInfo > 0) {
+                res.send(friendsList);
+            }
+            else {
+                res.send(Object.keys(friendsList));
+            }
+        });
 });
 
 app.get(/^\/bgm\-api\/getCollectionsList\/\d+/, function (req, res) {
-        var sid = findID(req.url);
-        superagent
-                .get('http://bangumi.tv/subject/' + sid + '/collections')
-                .end(function (err, data) {
-                        var txt = data.text;
-                });
-        res.send('I also want to konw who have done subject ' + sid);
+    // var sid = findID(req._parsedUrl.pathname);
+    var sid = findID(req.url);
+    superagent
+        .get('http://bangumi.tv/subject/' + sid + '/collections')
+        .end(function (err, data) {
+            var txt = data.text;
+        });
+    res.send('I also want to konw who have done subject ' + sid);
 });
 
 app.get(/^\/bgm\-api\/getDoingsList\/\d+/, function (req, res) {
-        var sid = findID(req.url);
-        superagent
-                .get('http://bangumi.tv/subject/' + sid + '/doings')
-                .end(function (err, data) {
-                        var txt = data.text;
-                });
-        res.send('I also want to konw who are doing subject '+ sid)
+    // var sid = findID(req._parsedUrl.pathname);
+    var sid = findID(req.url);
+    superagent
+        .get('http://bangumi.tv/subject/' + sid + '/doings')
+        .end(function (err, data) {
+            var txt = data.text;
+        });
+    res.send('I also want to konw who are doing subject '+ sid)
 });
 
 app.get('/*', function (req, res) {
-        res.send('who are you?');
+    res.send('who are you?');
 })
-
 
 app.listen(3000, function () {});
