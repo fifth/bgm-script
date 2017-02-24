bc5acbf5979246e91cc5055fcf9ae3ba9a9b8f24
Yu Jiechao <fifth0725@gmail.com>
2017-02-25 05:12:18 +0800

    version number change

--- subjectPowerUp.user.js
+++ subjectPowerUp.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         subjectPowerUp
 // @namespace    fifth26.com
-// @version      1.0.0
+// @version      1.0.1
 // @description  subject page power up ver2.0
 // @author       fifth(aslo thanks to everpcpc)
 // @include      /^https?://(bgm\.tv|chii\.in|bangumi\.tv)/subject/\d+$/
